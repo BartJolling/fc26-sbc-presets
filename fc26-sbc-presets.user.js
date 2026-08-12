d0f8dbff9631b7f3de1d8b03608554be7e34399c
Bart Jolling <BartJolling@users.noreply.github.com>
2026-08-12 10:49:41 +0200

    add 1 of 3 85+ PP and FUTTIES Provisions

--- fc26-sbc-presets.user.js
+++ fc26-sbc-presets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FC26 SBC Presets
 // @namespace    https://github.com/BartJolling/fc26-sbc-presets
-// @version      0.7
+// @version      0.8
 // @description  FC 26 FUT SBC helper
 // @author       BartJolling
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ea.com
